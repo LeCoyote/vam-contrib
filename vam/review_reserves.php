ba180c5ecba14b029e444a4c078b5e0d35b86fef
Romain Rivière <lecoyote@lecoyote.org>
2016-03-28 00:41:37 +0200

    Bugfix: reservations last for 24hrs instead of until midnight

--- vam/review_reserves.php
+++ vam/review_reserves.php
@@ -16,17 +16,15 @@
 	if ($db->connect_errno > 0) {
 		die('Unable to connect to database [' . $db->connect_error . ']');
 	}
-	$sql = "select id,DATE_FORMAT(date,'%Y%m%d') as reserve_date ,DATE_FORMAT(now(),'%Y%m%d') as currdat ,route_id, gvauser_id, fleet_id  from reserves";
+	$sql = "select id,TIMESTAMPDIFF(HOUR,date,NOW()) as diffhours,route_id, gvauser_id, fleet_id  from reserves";
 	if (!$result = $db->query($sql)) {
 		die('There was an error running the query  [' . $db->error . ']');
 	}
 	while ($row = $result->fetch_assoc()) {
 		$plane = $row["fleet_id"];
 		$pilot = $row["gvauser_id"];
-		$reserve_date = $row["reserve_date"];
-		$currdat = $row["currdat"];
-		$diffdays=intval ($reserve_date)- intval ($currdat);
-		if (intval($diffdays)<0)
+		$route = $row["route_id"];
+		if ($row['diffhours'] >= 24)
 		{
 			$sql1  = "update gvausers set route_id= NULL, book_date= NULL where gvauser_id=$pilot";
 			if (!$result1 = $db->query($sql1)) {
